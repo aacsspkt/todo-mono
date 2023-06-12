d20e51631c814256bb5a561fc6eabacca2364235
Ashish Sapkota <aacsspkt3@gmail.com>
2023-06-12 15:45:48 +0545

    deploy contract to bsc testnet

--- packages/todo-contract/scripts/deploy.ts
+++ packages/todo-contract/scripts/deploy.ts
@@ -1,8 +1,13 @@
+import * as fs from 'fs';
 import { ethers } from 'hardhat';
 
 async function main() { 
   const todos = await ethers.deployContract("Todos");
   await todos.waitForDeployment();
+
+  console.log("Todo contract deployed at:", todos.target);
+
+  fs.writeFileSync("contract.json", JSON.stringify({ contract: todos.target }));
 }
 
 // We recommend this pattern to be able to use async/await everywhere
