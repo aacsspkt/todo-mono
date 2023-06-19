2597a16b6dbd0dd63ce563e5dcc17504d3bb7d5e
Ashish Sapkota <aacsspkt3@gmail.com>
2023-06-19 10:49:42 +0545

    update contract

--- packages/todo-contract/contracts/Todos.sol
+++ packages/todo-contract/contracts/Todos.sol
@@ -3,76 +3,80 @@ pragma solidity ^0.8.9;
 
 contract Todos {
     struct Task {
-        uint id;
+        uint256 id;
         string task;
         bool completed;
         address owner;
     }
 
-    Task[] private tasks;
 
-    event TaskCreated(uint id, string task, bool completed);
-    event TaskUpdated(uint id, string task, bool completed);
-    event TaskToggleCompleted(uint id, string task, bool completed);
+    // Task[] private tasks;
+    mapping (address => Task[]) tasks;
+    uint256 count;
+
+
+    event TaskCreated(uint256 id, string task, bool completed, address owner);
+    event TaskUpdated(uint256 id, string task, bool completed, address owner);
+    event TaskToggleCompleted(uint256 id, string task, bool completed, address owner);
     event TaskDeleted(uint id);
 
     function addTask(string calldata _task) external {
-        uint taskId = tasks.length;
+        address owner = msg.sender;
+        Task memory task = Task({id: count, task: _task, completed: false, owner: msg.sender});
+        tasks[owner].push(task);
 
-        Task memory task = Task({ id: taskId, task: _task, completed: false, owner: msg.sender});
-        tasks.push(task);
+        emit TaskCreated(count, task.task, task.completed, owner);
 
-        emit TaskCreated(task.id, task.task, task.completed);
+        count += 1;
     }
 
     function getTasks() external view returns (Task[] memory result) {
-        Task[] memory temp = new Task[](tasks.length);
-        uint counter = 0;
-        
-        for(uint i = 0; i<tasks.length; i++){
-            if (tasks[i].owner == msg.sender) {
-                temp[counter] = tasks[i];
-                counter++;
-            }
-        }
-
-        result = new Task[](counter);
-        for (uint i=0; i<counter; i++) {
-            result[i] = temp[i];
-        }
+        result = tasks[msg.sender]; 
     }
 
     function getTask(uint _index) external view returns (Task memory task) {
-        require(tasks[_index].owner == msg.sender, "Unauthorized owner");
+        address owner = msg.sender;
+        Task[] memory taskOwned = tasks[owner];
+
+        require(_index < taskOwned.length, "Index out of bound");
         
-        task = tasks[_index];
+        task = taskOwned[_index];
     }
 
     function updateTask(uint _index, string calldata _task) external {
-        require(tasks[_index].owner == msg.sender, "Unauthorized owner");
-        
-        Task storage task = tasks[_index];
+        address owner = msg.sender;
+        Task[] storage taskOwned = tasks[owner];
+
+        require(_index < taskOwned.length, "Index out of bound");
+
+        Task storage task = taskOwned[_index];
         task.task = _task;
 
-        emit TaskUpdated(task.id, task.task, task.completed);
+        emit TaskUpdated(task.id, task.task, task.completed, owner);
     }
 
     function toggleCompleted(uint _index) external {
-        require(tasks[_index].owner == msg.sender, "Unauthorized owner");
+        address owner = msg.sender;
+        Task[] storage taskOwned = tasks[owner];
+
+        require(_index < taskOwned.length, "Index out of bound");
         
-        Task storage task = tasks[_index];
+        Task storage task = taskOwned[_index];
         task.completed = !task.completed;
 
-        emit TaskToggleCompleted(task.id, task.task, task.completed);
+        emit TaskToggleCompleted(task.id, task.task, task.completed, owner);
     }
 
     function deleteTask(uint _index) external {
-        require(_index < tasks.length, "Invalid index provided");
+        address owner = msg.sender;
+        Task[] storage taskOwned = tasks[owner];
+
+        require(_index < taskOwned.length, "Index out of bound");
         
-        for(uint i = _index; i < tasks.length-1; i++){
-            tasks[i] = tasks[i+1];      
+        for(uint i = _index; i < taskOwned.length-1; i++){
+            taskOwned[i] = taskOwned[i+1];      
         }
-        tasks.pop();
+        taskOwned.pop();
         
         emit TaskDeleted(_index);
     }
