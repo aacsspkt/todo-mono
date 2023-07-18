6f62f647b8ab8271e705e4ddd8633c7c414af65f
Ashish Sapkota <aacsspkt3@gmail.com>
2023-07-18 18:09:16 +0545

    remove update method and modify task struct

--- packages/todo-contract/contracts/Todos.sol
+++ packages/todo-contract/contracts/Todos.sol
@@ -3,31 +3,24 @@ pragma solidity ^0.8.9;
 
 contract Todos {
     struct Task {
-        uint256 id;
         string task;
         bool completed;
-        address owner;
     }
 
 
     // Task[] private tasks;
     mapping (address => Task[]) tasks;
-    uint256 count;
 
-
-    event TaskCreated(uint256 id, string task, bool completed, address owner);
-    event TaskUpdated(uint256 id, string task, bool completed, address owner);
-    event TaskToggleCompleted(uint256 id, string task, bool completed, address owner);
-    event TaskDeleted(uint id);
+    event TaskCreated(string task, bool completed, address owner);
+    event TaskToggleCompleted(string task, bool completed, address owner);
+    event TaskDeleted(uint256 id);
 
     function addTask(string calldata _task) external {
         address owner = msg.sender;
-        Task memory task = Task({id: count, task: _task, completed: false, owner: msg.sender});
+        Task memory task = Task({task: _task, completed: false});
         tasks[owner].push(task);
 
-        emit TaskCreated(count, task.task, task.completed, owner);
-
-        count += 1;
+        emit TaskCreated(task.task, task.completed, owner);
     }
 
     function getTasks() external view returns (Task[] memory result) {
@@ -43,18 +36,6 @@ contract Todos {
         task = taskOwned[_index];
     }
 
-    function updateTask(uint _index, string calldata _task) external {
-        address owner = msg.sender;
-        Task[] storage taskOwned = tasks[owner];
-
-        require(_index < taskOwned.length, "Index out of bound");
-
-        Task storage task = taskOwned[_index];
-        task.task = _task;
-
-        emit TaskUpdated(task.id, task.task, task.completed, owner);
-    }
-
     function toggleCompleted(uint _index) external {
         address owner = msg.sender;
         Task[] storage taskOwned = tasks[owner];
@@ -64,7 +45,7 @@ contract Todos {
         Task storage task = taskOwned[_index];
         task.completed = !task.completed;
 
-        emit TaskToggleCompleted(task.id, task.task, task.completed, owner);
+        emit TaskToggleCompleted(task.task, task.completed, owner);
     }
 
     function deleteTask(uint _index) external {
