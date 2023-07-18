4baf266479a9d41e37489655237ea656fb9fb359
Ashish Sapkota <aacsspkt3@gmail.com>
2023-07-18 23:45:56 +0545

    add todo list table and create form

--- packages/client/src/app/page.tsx
+++ packages/client/src/app/page.tsx
@@ -1,113 +1,162 @@
-import Image from 'next/image'
+"use client";
+
+import {
+  useCallback,
+  useEffect,
+  useMemo,
+  useState,
+} from 'react';
+
+import { useConnect } from 'wagmi';
+
+import { TODO_CONTRACT_ADDRESS } from '@/constants';
+import { Todos__factory } from '@/contract';
+import { useEthersSigner } from '@/hooks/useEthersSigner';
+import {
+  ParsedTodo,
+  parseTodo,
+} from '@/utils/parseTodos';
+
+type TaskFormInput = {
+	value: string;
+	error: string | null;
+};
 
 export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+	const [isSaving, setIsSaving] = useState(false);
+	const [task, setTask] = useState<TaskFormInput>({
+		value: "",
+		error: null,
+	});
+	const [parsedTodos, setParsedTodos] = useState<ParsedTodo[]>([]);
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+	const { connect } = useConnect();
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+	const signer = useEthersSigner();
+	const todos = useMemo(() => {
+		return signer ? Todos__factory.connect(TODO_CONTRACT_ADDRESS, signer) : undefined;
+	}, [signer]);
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+	const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+		setTask({
+			value: e.target.value,
+			error: null,
+		});
+	};
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
+	const handleSubmit = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+		e.preventDefault();
+		if (task.value.trim() === "") {
+			setTask({
+				...task,
+				error: "Task is empty",
+			});
+			return;
+		}
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+		if (signer == undefined) {
+			connect();
+		}
+
+		if (todos) {
+			setIsSaving(true);
+			const tx = await todos.addTask(task.value);
+			try {
+				const reciept = await tx.wait();
+				setIsSaving(false);
+			} catch (error) {}
+		}
+	};
+
+	const handleToggleCompleted = async (e: React.ChangeEvent<HTMLInputElement>) => {
+		const value = e.target.value;
+		console.log("toggled value", value);
+	};
+
+	const fetchTodos = useCallback(async () => {
+		const parsedTodos: ParsedTodo[] = [];
+		if (todos) {
+			const rawTodos = await todos.getTasks();
+
+			rawTodos.forEach((t) => {
+				parsedTodos.push(parseTodo(t));
+			});
+		}
+		return parsedTodos;
+	}, [todos]);
+
+	useEffect(() => {
+		fetchTodos().then((value) => setParsedTodos(value));
+	}, [fetchTodos]);
+
+	return (
+		<div className="max-w-6xl px-4 container mx-auto flex flex-col">
+			<div className="my-6">
+				<h1 className="text-4xl font-light">Todos</h1>
+			</div>
+			<div className="mb-4">
+				<form className="flex flex-row space-x-5">
+					<input
+						value={task.value}
+						onChange={handleTaskChange}
+						className="px-3 py-2 border-none rounded outline-none w-96"
+						placeholder="Enter Task"
+						type="text"
+						name="task"
+						required
+					/>
+					<button
+						disabled={isSaving}
+						className="w-32 hover:shadow-lg bg-emerald-600 text-white disabled:bg-emerald-300 rounded px-3 py-2"
+						type="submit"
+						onClick={handleSubmit}
+					>
+						{isSaving ? "Saving" : "Save"}
+					</button>
+				</form>
+			</div>
+			<div className="inline-block overflow-hidden">
+				<table className="min-w-full text-left text-sm font-light">
+					<thead className="border-b border-gray-700 font-medium">
+						<tr>
+							<th scope="col" className="px-6 py-3">
+								#
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Completed
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Task
+							</th>
+							<th scope="col" className="px-6 py-3">
+								Action
+							</th>
+						</tr>
+					</thead>
+					<tbody>
+						{parsedTodos.length > 0 &&
+							parsedTodos.map((t, i) => {
+								return (
+									<tr key={i} className="border-b border-gray-400 transition duration-300 ease-in-out hover:bg-neutral-100">
+										<td className="whitespace-nowrap px-6 py-3">{i + 1}</td>
+											<td className="whitespace-nowrap px-6 py-3">{t.task}</td>
+										<td className="whitespace-nowrap px-6 py-3">
+											<input
+												className="mr-2 mt-[0.3rem] h-3.5 w-8 appearance-none rounded-[0.4375rem] bg-neutral-300 before:pointer-events-none before:absolute before:h-3.5 before:w-3.5 before:rounded-full before:bg-transparent before:content-[''] after:absolute after:z-[2] after:-mt-[0.1875rem] after:h-5 after:w-5 after:rounded-full after:border-none after:bg-neutral-100 after:shadow-[0_0px_3px_0_rgb(0_0_0_/_7%),_0_2px_2px_0_rgb(0_0_0_/_4%)] after:transition-[background-color_0.2s,transform_0.2s] after:content-[''] checked:bg-primary checked:after:absolute checked:after:z-[2] checked:after:-mt-[3px] checked:after:ml-[1.0625rem] checked:after:h-5 checked:after:w-5 checked:after:rounded-full checked:after:border-none checked:after:bg-primary checked:after:shadow-[0_3px_1px_-2px_rgba(0,0,0,0.2),_0_2px_2px_0_rgba(0,0,0,0.14),_0_1px_5px_0_rgba(0,0,0,0.12)] checked:after:transition-[background-color_0.2s,transform_0.2s] checked:after:content-[''] hover:cursor-pointer focus:outline-none focus:ring-0 focus:before:scale-100 focus:before:opacity-[0.12] focus:before:shadow-[3px_-1px_0px_13px_rgba(0,0,0,0.6)] focus:before:transition-[box-shadow_0.2s,transform_0.2s] focus:after:absolute focus:after:z-[1] focus:after:block focus:after:h-5 focus:after:w-5 focus:after:rounded-full focus:after:content-[''] checked:focus:border-primary checked:focus:bg-primary checked:focus:before:ml-[1.0625rem] checked:focus:before:scale-100 checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca] checked:focus:before:transition-[box-shadow_0.2s,transform_0.2s] dark:bg-neutral-600 dark:after:bg-neutral-400 dark:checked:bg-primary dark:checked:after:bg-primary dark:focus:before:shadow-[3px_-1px_0px_13px_rgba(255,255,255,0.4)] dark:checked:focus:before:shadow-[3px_-1px_0px_13px_#3b71ca]"
+												type="checkbox"
+												role="switch"
+												checked={t.completed}
+												title="task completed"
+												onChange={handleToggleCompleted}
+											/>
+										</td>
+										<td className="whitespace-nowrap px-6 py-3">Delete</td>
+									</tr>
+								);
+							})}
+					</tbody>
+				</table>
+			</div>
+		</div>
+	);
 }
---
+++ packages/client/src/constants.ts
@@ -0,0 +1,6 @@
+
+export const DEFAULT_RPC = "https://data-seed-prebsc-1-s3.binance.org:8545";
+
+export const BSC_TESTNET_CHAIN_ID = 97;
+
+export const TODO_CONTRACT_ADDRESS = "0xb85A51f35A376d80eBf04eB801CED9fAD106e4d8";
